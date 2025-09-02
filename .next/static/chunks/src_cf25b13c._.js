ececc2055463e18be4513a4043a27e7182850f87
SHIVA VANAM <shivakumarvanam27@gmail.com>
2025-09-02 05:31:20 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Features>
    
    with t

--- .next/static/chunks/src_cf25b13c._.js
+++ .next/static/chunks/src_cf25b13c._.js
@@ -1457,7 +1457,7 @@ function Features() {
                         }, this),
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
                             className: "text-lg md:text-xl text-muted-foreground mt-4 max-w-3xl mx-auto",
-                            children: "Discover what makes our free tuition centre a special place for learning and growth."
+                            children: "Opening doors of learning, free for all"
                         }, void 0, false, {
                             fileName: "[project]/src/components/sections/features.tsx",
                             lineNumber: 45,
