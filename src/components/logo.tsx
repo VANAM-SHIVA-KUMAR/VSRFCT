737f81b8f5fcad801c293f947a533beafb35d667
SHIVA VANAM <shivakumarvanam27@gmail.com>
2025-09-05 05:42:06 +0000

    decrease the size and it should match with the logo size and fit to the

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -12,10 +12,10 @@ export function Logo() {
         data-ai-hint="logo"
       />
       <div>
-        <p className="text-4xl font-bold font-headline text-green-900">
+        <p className="text-2xl font-bold font-headline text-green-900">
           Prof. V.S. Raju
         </p>
-        <p className="text-4xl font-bold font-headline text-green-700">
+        <p className="text-xl font-bold font-headline text-green-700">
           Family Charitable Trust
         </p>
       </div>
