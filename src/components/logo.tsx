dabeba9777809cded3321b0986a86429f3b6421e
SHIVA VANAM <shivakumarvanam27@gmail.com>
2025-09-09 06:29:04 +0000

    remove colour and match with website (_for element p_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,11 +8,11 @@ export function Logo() {
         alt="VSRFCT Logo"
         width={120}
         height={120}
-        className="h-14 w-14 md:h-20 md:w-20 rounded-full"
+        className="h-14 w-14 md:h-16 md:w-16 rounded-full"
         data-ai-hint="logo"
       />
       <div>
-        <p className="text-xl md:text-2xl font-bold font-headline text-green-900">
+        <p className="text-xl md:text-2xl font-bold font-headline text-primary">
           Prof. V.S. Raju
         </p>
         <p className="text-sm md:text-lg font-bold font-headline text-green-300">
