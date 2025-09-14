8ecf867bcce199187c1fa1db4d30615aae0b4ab8
SHIVA VANAM <shivakumarvanam27@gmail.com>
2025-09-14 17:43:59 +0000

    In about us also Branch title and Staff details must be present

--- .next/static/chunks/src_app_branches_page_tsx_d846e3da._.js
+++ .next/static/chunks/src_app_branches_page_tsx_d846e3da._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_e3d508fc._.js",
-  "static/chunks/src_e8c8dc85._.js"
+  "static/chunks/src_2cdc2d93._.js",
+  "static/chunks/node_modules_be5f6e0f._.js"
 ],
     source: "dynamic"
 });
