02b9fd248d288227ab0a502dd5b66456823b5fad
VANAM SHIVA KUMAR <127430594+VANAM-SHIVA-KUMAR@users.noreply.github.com>
2025-10-10 11:07:50 +0530

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,8 +3,8 @@
 import Header from '@/components/sections/header';
 import Footer from '@/components/sections/footer';
 import About from '@/components/sections/about';
-import VempaduTeam from '@/components/sections/vempadu-team';
 import TelecomnagarTeam from '@/components/sections/team';
+import VempaduTeam from '@/components/sections/vempadu-team';
 import KopalleTeam from '@/components/sections/kopalle-team';
 
 export default function AboutPage() {
@@ -18,16 +18,16 @@ export default function AboutPage() {
             <h2 className="text-3xl md:text-5xl font-bold font-headline text-green-900">Vempadu Branch</h2>
           </div>
         </div>
-        <VempaduTeam />
-        <div className="container mx-auto px-4 pt-12 md:pt-24">
-          <div className="text-center mb-12">
-            <h2 className="text-3xl md:text-5xl font-bold font-headline text-green-900">Telecomnagar Branch</h2>
-          </div>
-        </div>
         <TelecomnagarTeam />
         <div className="container mx-auto px-4 pt-12 md:pt-24">
           <div className="text-center mb-12">
             <h2 className="text-3xl md:text-5xl font-bold font-headline text-green-900">Kopalle Branch</h2>
+          </div>
+        </div>
+        <VempaduTeam />
+        <div className="container mx-auto px-4 pt-12 md:pt-24">
+          <div className="text-center mb-12">
+            <h2 className="text-3xl md:text-5xl font-bold font-headline text-green-900">Telecomnagar Branch</h2>
           </div>
         </div>
         <KopalleTeam />
